730583a4df0c29b2ff8556871af65bffbf7ec953
Marc Monfort <marc.monfort@gmail.com>
2020-06-12 00:22:41 +0200

    vehiculo posicion

--- SmartCity/src/org/upc/edu/Behaviours/EntornoAgent.java
+++ SmartCity/src/org/upc/edu/Behaviours/EntornoAgent.java
@@ -7,15 +7,38 @@ package org.upc.edu.Behaviours;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.domain.FIPANames;
 import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.proto.AchieveREResponder;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Random;
 
 /**
  * @author igomez
  */
 public class EntornoAgent extends Agent {
+
+    class Vehiculo {
+        public int pos_x;
+        public int pos_y;
+        public int direccion;
+        public int velocidad;
+
+    };
+
+    //private ArrayList<Vehiculo> info_vehiculos;
+
+    private HashMap<Integer, Vehiculo> info_vehiculo = new HashMap<Integer, Vehiculo>();
 
     public class EntornoTickerBehaviour extends TickerBehaviour {
 
@@ -39,6 +62,119 @@ public class EntornoAgent extends Agent {
         public void onTick() {
             System.out.println("Vehiculo: Posicion, Velocidad, Dirección"); // print vector de info vehiculos
             System.out.println("Semaforo: Color"); // print vector de info semaforo
+
+            // AQUI SI FUNCIONA CON MULTIPLES AGENTES, ¿¿¿PORQUE???
+            MessageTemplate mt = AchieveREResponder.createMessageTemplate(FIPANames.InteractionProtocol.FIPA_QUERY);
+            myAgent.addBehaviour(new AchieveREResponder(myAgent, mt) {
+                protected ACLMessage prepareResultNotification(ACLMessage request, ACLMessage response) {
+                    
+                    // 2 posibilities (accident, nextobstacle)
+                    String[] contentArray = request.getContent().split(",");
+                    int elID = Integer.parseInt(contentArray[0]);
+
+                    Vehiculo aux = new Vehiculo();
+                    aux.pos_x = Integer.parseInt(contentArray[1]);
+                    aux.pos_y = Integer.parseInt(contentArray[2]);
+                    aux.velocidad = Integer.parseInt(contentArray[3]);
+                    aux.direccion = Integer.parseInt(contentArray[4]);
+
+                    info_vehiculo.put(elID, aux);
+
+                    ACLMessage informDone  = request.createReply();
+                    informDone.setPerformative(ACLMessage.INFORM);
+                    informDone.setContent(request.getSender().getName());
+                    System.out.println("recibido " + request.getSender().getName());
+                    return informDone;
+                }
+                protected ACLMessage handleRequest(ACLMessage request) {
+                    return null;
+            }
+            });
+
+
+            //recorremos info_vehiculo para poner el estado actual.
+            for (Integer i : info_vehiculo.keySet()) {
+                Vehiculo v = info_vehiculo.get(i);
+                System.out.println(i + ": " + v.pos_x + "," + v.pos_y + "," + v.velocidad + "," + v.direccion );
+            }
+        }
+
+    }
+
+    public class EntornoCyclicBehaviour extends CyclicBehaviour {
+
+        //MessageTemplate tmpl;
+
+        public EntornoCyclicBehaviour() {
+        }
+
+        /* public void onStart() {
+            tmpl = MessageTemplate.MatchSender((new AID("Termometro", AID.ISLOCALNAME)));
+        }
+
+        public int onEnd() {
+            System.out.println("Bye..");
+            return 0;
+        } */
+
+        public void action() {
+            final ACLMessage request = receive(); //usar filtro??
+
+            if (request != null) {
+                //if(request.get)
+                /* final AID sender = request.getSender();
+                final ACLMessage reply = request.createReply(); */
+                System.out.println("recibido algo...");
+            }
+            else {
+                block();
+            }
+
+
+            /* try {
+                final DFAgentDescription desc = new DFAgentDescription();
+                desc.setName(sender);
+                final DFAgentDescription[] search = DFService.search(EntornoAgent.this, getDefaultDF(), desc);
+                final Iterator services = search[0].getAllServices();
+                final ServiceDescription service = (ServiceDescription) services.next();
+                String content = request.getContent();
+                String[] contentArray = content.split(",");
+                //float volumeDischarged = Float.valueOf(contentArray[2]);
+                //float concentrationDischarged = Float.valueOf(contentArray[4].replace(")", ""));
+                if (true) {
+                    // DEBUG
+                    //illegalDischargesDetected += 1;
+                    //
+                    reply.setPerformative(ACLMessage.REQUEST);
+                    reply.setContent("papito");
+                } else {
+                    reply.setPerformative(ACLMessage.INFORM);
+                }
+                EntornoAgent.this.send(reply);
+
+
+                /* float currentMassOfPollutant = currentVolume * currentConcentration;  // Mass in g
+                float massOfPollutantDischarged = volumeDischarged * concentrationDischarged;  // Mass in g
+                float totalMassOfPollutant = currentMassOfPollutant + massOfPollutantDischarged;  // Mass in g
+                currentVolume += volumeDischarged;  // Volume in m3
+                currentConcentration = totalMassOfPollutant / currentVolume;  // Concentration in g/m3 */
+
+            /*} catch (FIPAException e) {
+                e.printStackTrace();
+            } */
+
+            /* ACLMessage msg = receive(tmpl);
+            if (msg != null) {
+                int n = Integer.parseInt(msg.getContent());
+
+                if (n < 15) System.out.println("calefacción encendida (" + n + "°C)");
+                else if (n > 25) System.out.println("refrigeración encendida (" + n + "°C)");
+                else System.out.println("temperatura adecuada (" + n + "°C)");
+            }
+            else {
+                block();
+            } */
+            //System.out.println("aaa");
         }
 
     }
@@ -60,7 +196,26 @@ public class EntornoAgent extends Agent {
         }
         // FIN REGISTRO DF
 
+
+        /* MessageTemplate mt = AchieveREResponder.createMessageTemplate(FIPANames.InteractionProtocol.FIPA_QUERY);
+        this.addBehaviour(new AchieveREResponder(this, mt) {
+            protected ACLMessage prepareResultNotification(ACLMessage request, ACLMessage response) {
+
+                ACLMessage informDone  = request.createReply();
+                informDone.setPerformative(ACLMessage.INFORM);
+                informDone.setContent(request.getSender().getName());
+                System.out.println("recibido " + request.getSender().getName());
+                return informDone;
+            }
+            protected ACLMessage handleRequest(ACLMessage request) {
+                return null;
+            }
+        }); */
+
         EntornoTickerBehaviour b = new EntornoTickerBehaviour(this, 3000);
         this.addBehaviour(b);
+
+        EntornoCyclicBehaviour b2 = new EntornoCyclicBehaviour();
+        this.addBehaviour(b2);
     }
 }
--- SmartCity/src/org/upc/edu/Behaviours/VehiculoAgent.java
+++ SmartCity/src/org/upc/edu/Behaviours/VehiculoAgent.java
@@ -8,7 +8,14 @@ package org.upc.edu.Behaviours;
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.SearchConstraints;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.domain.FIPANames;
 import jade.lang.acl.ACLMessage;
+import jade.proto.AchieveREInitiator;
 
 import java.util.Random;
 
@@ -22,11 +29,21 @@ public class VehiculoAgent extends Agent {
 
     int direccion = 0;
 
-    int velocidad = 0;
-    int aceleracion = 1;
+    int velocidad = 0; // velocidad 1 o 0
+    //int aceleracion = 1;
 
-    int carril_fin = 50; //fin de carril
+    int carril_fin_x = 2;
+    int carril_fin_y = 0; 
+
     int[] carril_inter = {20,30};
+
+    int dist_next_obstacle = 0; //-1 == infinite
+
+    boolean all_green = true;
+
+    int iter = 3;
+
+    int elID;
 
 
 
@@ -51,24 +68,126 @@ public class VehiculoAgent extends Agent {
 
         public void onTick() {
             // mirar si se llega al final del carril
+            boolean fin_calle = false;
+
             switch(direccion) {
-                case 0:
-                    pos_x += velocidad; break;
-                case 1:
-                    pos_x -= velocidad; break;
-                case 2:
-                    pos_y += velocidad; break;
-                case 3:
-                    pos_y -= velocidad; break;
+                case 0: //right
+                    pos_x += velocidad; 
+                    break;
+                case 1: //left
+                    pos_x -= velocidad; 
+                    break;
+                case 2: //up
+                    pos_y += velocidad; 
+                    break;
+                case 3: //down
+                    pos_y -= velocidad; 
+                    break;
             }
-            velocidad += aceleracion;
-            System.out.println("Vehiculo: Posicion=("+pos_x+", "+pos_y+"), Velocidad="+velocidad+", Dirección="+direccion);
+            if (pos_x == carril_fin_x && pos_y == carril_fin_y){
+                velocidad = 0;
+                // preguntar siguiente carretera
+            }
+            if (dist_next_obstacle == 1) {
+                velocidad = 0;
+            }
+
+            
+            --dist_next_obstacle; // reducimos distancia al objeto mas cercano
+            
+            // Establece posicion y recibe next obstacle en cada tick
+            if (true) {
+                final DFAgentDescription desc = new DFAgentDescription();
+                final ServiceDescription sdesc = new ServiceDescription();
+                sdesc.setType("Entorno");
+                desc.addServices(sdesc);
+                try {
+                    final DFAgentDescription[] environments = DFService.search(VehiculoAgent.this, getDefaultDF(), desc, new SearchConstraints());
+                    final AID environment = environments[0].getName();
+                    final ACLMessage aclMessage = new ACLMessage(ACLMessage.QUERY_IF);
+
+                    aclMessage.setProtocol(FIPANames.InteractionProtocol.FIPA_QUERY);
+                    aclMessage.setSender(VehiculoAgent.this.getAID());
+                    aclMessage.addReceiver(environment);
+                    aclMessage.setContent(elID + "," + pos_x + "," + pos_y + "," + velocidad + "," + direccion);
+
+                    myAgent.addBehaviour(new AchieveREInitiator(myAgent, aclMessage) {
+                        //@override???
+                        protected void handleInform(ACLMessage inform) {
+                            //double t = Double.parseDouble(inform.getContent());
+                            System.out.println(VehiculoAgent.this.getName() + " == " + inform.getContent()); //
+                            
+                        }
+                    });
+                    
+                    
+                    //VehiculoAgent.this.send(aclMessage);
+                    
+                    System.out.println("ha superado el send!");
+                    iter = 4;
+                } catch (FIPAException e) {
+                    e.printStackTrace();
+                }
+            }
+            //--iter;
+
+
+            //velocidad += aceleracion;
+            //System.out.println("Vehiculo: Posicion=("+pos_x+", "+pos_y+"), Velocidad="+velocidad+", Dirección="+direccion);
+
+
             //System.out.println("hola");
+            /* if (true) { //request avanzar
+                final DFAgentDescription desc = new DFAgentDescription();
+                final ServiceDescription sdesc = new ServiceDescription();
+                sdesc.setType("Entorno");
+                desc.addServices(sdesc);
+                try {
+                    final DFAgentDescription[] environments = DFService.search(VehiculoAgent.this, getDefaultDF(), desc, new SearchConstraints());
+                    final AID environment = environments[0].getName();
+                    final ACLMessage aclMessage = new ACLMessage(ACLMessage.REQUEST);
+                    aclMessage.setSender(VehiculoAgent.this.getAID());
+                    aclMessage.addReceiver(environment);
+                    aclMessage.setContent(pos_x + "," + pos_y + "," + velocidad + "," + direccion);
+                    VehiculoAgent.this.send(aclMessage);
+                    
+                    System.out.println("ha superado el send!");
+                } catch (FIPAException e) {
+                    e.printStackTrace();
+                }
+            } */
+
+            // repetido
+            /* if (true) { //query algun obstaculo???
+                final DFAgentDescription desc = new DFAgentDescription();
+                final ServiceDescription sdesc = new ServiceDescription();
+                sdesc.setType("Entorno");
+                desc.addServices(sdesc);
+                try {
+                    final DFAgentDescription[] environments = DFService.search(VehiculoAgent.this, getDefaultDF(), desc, new SearchConstraints());
+                    final AID environment = environments[0].getName();
+                    final ACLMessage aclMessage = new ACLMessage(ACLMessage.QUERY_IF);
+                    aclMessage.setSender(VehiculoAgent.this.getAID());
+                    aclMessage.addReceiver(environment);
+                    aclMessage.setContent("obstacle");
+                    VehiculoAgent.this.send(aclMessage);
+                    
+                    System.out.println("hay obstaculo?");
+                } catch (FIPAException e) {
+                    e.printStackTrace();
+                }
+            } */
+
+
         }
 
     }
 
     protected void setup() {
+        Object[] args = getArguments();
+        elID = Integer.parseInt((String) args[0]);
+        System.out.println(elID);
+
         // REGISTRO DF
         final DFAgentDescription desc = new DFAgentDescription();
         desc.setName(getAID());
